7b3be80317ac2de31e02a808729dcf3d29494816
Nicholas Pica <nicapica@ut.utm.edu>
2019-02-05 14:27:22 -0600

    WIP

---
+++ AdapterPattern/Grizzly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class Grizzly : Bear
+    {
+        void Bear.Hibernate()
+        {
+            Console.WriteLine("The Bear Hibernates");
+        }
+
+        void Bear.Maul()
+        {
+            Console.WriteLine("The Bear Completely destroys you....you're dead");
+        }
+    }
+}
---
+++ AdapterPattern/TeddyBear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    class TeddyBear : ToyBear
+    {
+        void ToyBear.Hug()
+        {
+            Console.WriteLine("The toy bear hugs you");
+        }
+    }
+}
